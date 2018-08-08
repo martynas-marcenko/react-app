8b1827de354bc571f148d44e21da9038d54a272e
Martynas Marcenko <martynasmarcenko@Martynass-iMac.local>
2018-08-08 21:18:46 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "rules": {
+            "react/jsx-filename-extension": [1, {
+                "extensions": [".js", ".jsx"]
+            }],
+        }
+    }
+};
